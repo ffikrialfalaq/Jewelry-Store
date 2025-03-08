63b602c4452c09f3ce83db4b8fe368fc672dd41a
Draco <ffikrialfalaq@gmail.com>
2025-03-08 12:16:55 +0700

    Homepage

---
+++ Js/Script.js
@@ -0,0 +1,13 @@
+// JavaScript for sticky navbar effect
+window.onscroll = function() {stickyNavbar()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function stickyNavbar() {
+    if (window.pageYOffset > sticky) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
